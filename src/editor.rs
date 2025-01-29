cd6d7a1afecb6b077bf02b1dad9b120b79ad404a
Ahmed <ahmed.aden14@icloud.com>
2025-01-29 22:25:42 +1100

    tildes

--- src/editor.rs
+++ src/editor.rs
@@ -1,16 +1,22 @@
-use crossterm::event::{read, Event::Key, KeyCode::Char};
-use crossterm::terminal::{disable_raw_mode, enable_raw_mode};
+
+use crossterm::event::{read, Event::Key, KeyCode::Char, KeyEvent, KeyModifiers};
+use crossterm::execute;
+use std::io::{self, Write};
+use crossterm::terminal::{disable_raw_mode, enable_raw_mode, size};
+use crossterm::cursor::{MoveTo};
 
 
 
-pub struct Editor {}
+pub struct Editor {
+    should_quit: bool,
+}
 
 impl Editor {
     pub fn default() -> Self {            
-        Editor{}            
+        Editor{ should_quit: false }             
     }
 
-    pub fn run(&self) {
+    pub fn run(&mut self) {
         if let Err(err) = self.repl() {
             panic!("{err:#?}");
         }
@@ -18,20 +24,41 @@ impl Editor {
     }
 
 
+    fn draw_rows() {
+        // fix error handling here
+        let ( _, rows) = size().unwrap_or_else(|e| {
+            eprintln!("error getting terminal size {:?}", e);
+            (0, 10)
+        });
+        let mut stdout = io::stdout();
+        for number in 0 .. rows {
+            // should print out smth like 1-10?
+            execute!(stdout, MoveTo(0, number)).unwrap();
+            println!("~");
+            stdout.flush().unwrap();
+            
+        }
+    }
 
 
-    fn repl(&self) -> Result<(), std::io::Error> {
+    fn repl(&mut self) -> Result<(), std::io::Error> {
         enable_raw_mode()?;
+        Self::draw_rows();
         loop {
-           if let Key(event) = read()? {
-                println!("{event:?} \r");
-                if let Char(c) = event.code {
-                    if c == 'q' {
-                        break;
+            
+           if let Key(KeyEvent {
+            code, modifiers, kind, state
+           }) = read()? {
+                println!("Code: {code:?} Modifiers: {modifiers:?} Kind : {kind:?} State: {state:?} \r");
+                match code {
+                    Char('x') if modifiers == KeyModifiers::CONTROL => { 
+                        self.should_quit = true; 
                     }
+                    _ => (),
                 }
-
-
+            }
+            if self.should_quit {
+                break;
             }
         }
         disable_raw_mode()?; 
